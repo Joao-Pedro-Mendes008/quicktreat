95487669c0b0116c6e95d3d70da3af8a5fd04534
João Pedro <joaopedromendesfernandes08@gmail.com>
2025-09-04 22:00:03 -0300

    testes de conexão com o banco, sem sucesso.

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-module.exports = app
+const { createClient } = require('@supabase/supabase-js');
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+app.get('/pacientes', (req, res) => {
+res.send('Hello World!');
+});
+
+app.get('/pacientes', async (req, res) => {
+  const { data, error } = await supabase
+    .from('pacientes')
+    .select('*');
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    } else {
+      res.send(data);
+    }
+});
+
+module.exports = app;
+
 
