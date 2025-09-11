ccb2b71354543fee90d633b5c83b73eb33a5932c
João Pedro <joaopedromendesfernandes08@gmail.com>
2025-09-10 21:09:58 -0300

    Inicio da estilização e mudanças na opção de adicionar paciente, agora recarregando a pagina após adicionar um paciente.

--- quicktreat/src/app/lista/page.js
+++ quicktreat/src/app/lista/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import '../lista/teste.css'
 import { useEffect, useState } from "react";
 
 export default function Pacientes() {
@@ -38,6 +39,7 @@ export default function Pacientes() {
 
       const data = await res.json();
       if (!res.ok) throw new Error(data.error || 'Erro ao adicionar paciente');
+      location.reload()
       setPacientes(...pacientes, data.data[0]);
 
       setNome("");
@@ -46,7 +48,6 @@ export default function Pacientes() {
       setDtNasc("");
       setEndereco("");
       setTelefone("");
-
     } catch (err) {
 
     }
@@ -56,48 +57,51 @@ export default function Pacientes() {
     <div>
       <h1>Pacientes</h1>
       <form onSubmit={addPacientes}>
-        <input
-          type="text"
-          placeholder="Nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}                      // Valor do input controlado pelo estado email
-          onChange={(e) => setEmail(e.target.value)} // Atualiza estado email a cada digitação
-          className="border p-2 rounded w-full"
-        />
-        <input
-          type="text"
-          placeholder="Telefone"
-          value={telefone}                      // Valor do input controlado pelo estado email
-          onChange={(e) => setTelefone(e.target.value)} // Atualiza estado email a cada digitação
-          className="border p-2 rounded w-full"
-        />
-        <input
-          type="text"
-          placeholder="CPF"
-          value={cpf}                      // Valor do input controlado pelo estado email
-          onChange={(e) => setCPF(e.target.value)} // Atualiza estado email a cada digitação
-          className="border p-2 rounded w-full"
-        />
-        <input
-          type="date"
-          placeholder="Data de Nascimento"
-          value={dtNasc}                      // Valor do input controlado pelo estado email
-          onChange={(e) => setDtNasc(e.target.value)} // Atualiza estado email a cada digitação
-          className="border p-2 rounded w-full"
-        />
-        <input
-          type="text"
-          placeholder="Endereço"
-          value={endereco}                      // Valor do input controlado pelo estado email
-          onChange={(e) => setEndereco(e.target.value)} // Atualiza estado email a cada digitação
-          className="border p-2 rounded w-full"
-        />
-        <button type="submit">
+        <div className="container">
+          <input
+            type="text"
+            placeholder="Nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+            className="teste"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}                      // Valor do input controlado pelo estado email
+            onChange={(e) => setEmail(e.target.value)} // Atualiza estado email a cada digitação
+            className="teste"
+          />
+          <input
+            type="text"
+            placeholder="Telefone"
+            value={telefone}                      // Valor do input controlado pelo estado email
+            onChange={(e) => setTelefone(e.target.value)} // Atualiza estado email a cada digitação
+            className="teste"
+          />
+          <input
+            type="text"
+            placeholder="CPF"
+            value={cpf}                      // Valor do input controlado pelo estado email
+            onChange={(e) => setCPF(e.target.value)} // Atualiza estado email a cada digitação
+            className="teste"
+          />
+          <input
+            type="date"
+            placeholder="Data de Nascimento"
+            value={dtNasc}                      // Valor do input controlado pelo estado email
+            onChange={(e) => setDtNasc(e.target.value)} // Atualiza estado email a cada digitação
+            className="teste"
+          />
+          <input
+            type="text"
+            placeholder="Endereço"
+            value={endereco}                      // Valor do input controlado pelo estado email
+            onChange={(e) => setEndereco(e.target.value)} // Atualiza estado email a cada digitação
+            className="teste"
+          />
+        </div>
+        <button type="submit" className='botaoEnviar'>
           Adicionar
         </button>
       </form>
